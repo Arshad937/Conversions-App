917de48ef34d1c5488f44e41a98dce1927945d6b
arshad <ak849496@gmail.com>
2023-12-08 15:53:53 +0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Arshad Mustafa on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
